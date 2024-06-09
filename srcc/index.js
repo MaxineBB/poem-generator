080631ed545e1770d26ccb7d99924519082b36a9
Max <161092590+MaxineBB@users.noreply.github.com>
2024-06-08 19:49:40 -0700

    Improved the user experience

--- srcc/index.js
+++ srcc/index.js
@@ -17,8 +17,12 @@ function generatePoem(event) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "cae984oa7de9d242032b1t958df34cd2";
     let prompt = `generate cozy themed short poem about ${instructionsInput.value}`;
-    let context = "Provide sweet and short cozy poems. Include a bold title at the top. Make sure to separate the title and each line with a <br />";
+    let context = "Provide sweet and short cozy poems with a bold title at the top. Make sure to separate the title and each line with a <br />";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<span class="blink">Poem about ${instructionsInput.value} is blooming quietly. 🌷</span>`;
 
     console.log("generating poem...");
     console.log(`prompt: ${prompt}`);
